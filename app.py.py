0f0bb96527d7affaa792166e6b3fe8e21f4c9250
fedecasilda98-art <fedecasilda98@gmail.com>
2026-05-17 13:14:38 -0300

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -184,7 +184,7 @@ with tabs[1]: # 🚚 LOTE
             if nuevo_costo > 0:
                 df_stock.at[idx, "Costo Base"] = nuevo_costo
                 flete = df_stock.at[idx, "Flete"]
-                 ganancia = df_stock.at[idx, "% Ganancia"]
+                ganancia = df_stock.at[idx, "% Ganancia"]
                 l1 = (nuevo_costo + flete) * (1 + ganancia / 100)
                 df_stock.at[idx, "Lista 1 (Cheques)"] = round(l1, 2)
                 df_stock.at[idx, "Lista 2 (Efectivo)"] = round(l1 * 0.90, 2)
